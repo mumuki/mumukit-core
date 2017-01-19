f830e1535912325726414f3a5825e1ddcfcc85e2
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-01-19 12:52:24 -0300

    Adding rspec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,6 @@
 require 'codeclimate-test-reporter'
 SimpleCov.start
 
-RSpec::Matchers.define :json_like do |expected, options={}|
-  except = options[:except] || []
-  match do |actual|
-    actual.as_json.with_indifferent_access.except(except) == expected.as_json.with_indifferent_access
-  end
-
-  failure_message_for_should do |actual|
-    <<-EOS
-    expected: #{expected.as_json} (#{expected.class})
-         got: #{actual.as_json} (#{actual.class})
-    EOS
-  end
-
-  failure_message_for_should_not do |actual|
-    <<-EOS
-    expected: value != #{expected.as_json} (#{expected.class})
-         got:          #{actual.as_json} (#{actual.class})
-    EOS
-  end
-end
-
+require 'mumukit/core/rspec'
 require 'mumukit/core'
 
