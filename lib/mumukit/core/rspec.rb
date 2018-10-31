210fe2d07f972b40a5b9a4a9a372b8c960f7160b
Franco Bulgarelli <franco@mumuki.org>
2018-10-31 18:05:34 -0300

    Allowing rspec helpers to properly work with v3

--- lib/mumukit/core/rspec.rb
+++ lib/mumukit/core/rspec.rb
@@ -13,18 +13,26 @@
       as_matcher_json(actual, options) == as_matcher_json(expected, options)
     end
 
-    failure_message_for_should do |actual|
+    message = proc do |actual|
       <<-EOS
       expected: #{as_matcher_json(expected, options)} (#{expected.class})
            got: #{as_matcher_json(actual, options)} (#{actual.class})
       EOS
     end
 
-    failure_message_for_should_not do |actual|
+    negated_message = proc do |actual|
       <<-EOS
       expected: value != #{as_matcher_json(expected, options)} (#{expected.class})
            got:          #{as_matcher_json(actual, options)} (#{actual.class})
       EOS
+    end
+
+    if respond_to? :failure_message
+      failure_message(&message)
+      failure_message_when_negated(&message)
+    else
+      failure_message_for_should(&:message)
+      failure_message_for_should_not(&:message)
     end
   end
 
