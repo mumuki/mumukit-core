6ff2fe6eeea18b2d3eaec0f1684da3201bb1e322
Julián Berbel Alt <julianberbel@hotmail.com>
2020-01-23 10:35:28 -0300

    Add child contract upholder tests

--- spec/mumukit/class_spec.rb
+++ spec/mumukit/class_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Class do
   describe '.implements' do
     let(:some_class) { Class.new { implements :foo } }
+    let(:some_module) { Module.new { implements :bar } }
 
     context 'when class declares it implements contract but breaches it' do
       it { expect { some_class.new }.to raise_error Class::ContractBreachError }
@@ -22,8 +23,23 @@ describe Class do
       it { expect(other_class.contract_breaches).to eq %i(foo) }
     end
 
+    context 'when inherited contract is upholded by child' do
+      let(:other_class) { Class.new(some_class) { def foo; end } }
+
+      it { expect { other_class.new }.to_not raise_error }
+      it { expect(some_class.contract_breaches).to eq %i(foo) }
+      it { expect(other_class.contract_breaches).to be_empty }
+    end
+
+    context 'when included contract is upholded by child' do
+      let(:other_class) { Class.new { def bar; end }.include some_module }
+
+      it { expect { other_class.new }.to_not raise_error }
+      it { expect(some_module.contract_breaches).to eq %i(bar) }
+      it { expect(other_class.contract_breaches).to be_empty }
+    end
+
     context 'when contract is included it validates ancestor\'s contract too' do
-      let(:some_module) { Module.new { implements :bar } }
       let(:other_class) { Class.new.include some_module }
 
       it { expect { other_class.new }.to raise_error Class::ContractBreachError }
