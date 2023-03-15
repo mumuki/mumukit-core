c3ae155801250693d685d0aba10713329d738258
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-03-15 01:09:26 -0300

    Remove contract stuff

--- lib/mumukit/core/module.rb
+++ lib/mumukit/core/module.rb
@@ -94,25 +94,4 @@ class Module
     define_method selector, &block
   end
 
-  def contract
-    @contract ||= []
-  end
-
-  attr_writer :contract
-
-  def implements(*selectors)
-    self.contract += selectors
-  end
-
-  def full_contract
-    ancestors.flat_map(&:contract).uniq
-  end
-
-  def breaches_contract?
-    !contract_breaches.empty?
-  end
-
-  def contract_breaches
-    full_contract.reject { |it| method_defined? it }
-  end
 end
